2e4ac251af93d3a5bbaabb06e7809e59253a2c28
islam kamel <islam.kamel@agr.svu.edu.eg>
2023-02-16 07:43:42 +0200

    Init User, Navbar, Footer and main layout Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './Components/user/user.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainLayoutComponent,
   ],
   imports: [
     BrowserModule,
